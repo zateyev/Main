cf8a8791f89827b7f2f59ca4b2dffa28fc775ac1
zateyev <zhasulan.ateyev@gmail.com>
2015-05-14 03:13:20 +0600

    Necessary to finalize

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
+/*import java.util.ArrayList;
+import java.util.List;*/
 
 public class Main {
     public static void main(String[] args) {
-
+        Matrix A = new Matrix(2,2);
+        System.out.println("Matrix A");
+        A.Print();
+        Matrix B = new Matrix(2,2);
+        System.out.println("Matrix B");
+        B.Print();
+        System.out.println("Matrix multiplication");
+        A.Multiply(B);
+        A.Print();
     }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,26 +1,46 @@
 import java.util.Random;
 
 public class Matrix {
-    int a[][];
+    int A[][];
     int m,n;
+    public Matrix(){
+        this.m=0;
+        this.n=0;
+    }
     public Matrix(int m, int n)
     {
         this.m=m;
         this.n=n;
-        Random r=new Random();
-        this.a=new int[m][n];
+        Random r = new Random();
+        this.A = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                a[i][j]=r.nextInt();
+                A[i][j]=r.nextInt()%11;
             }
         }
     }
     public void Print() {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.println(a[i][j]+" ");
+                System.out.print(A[i][j] + "\t");
             }
             System.out.println("\n");
         }
+    }
+    public Matrix Multiply(Matrix c){
+        if(this.n!=c.m){
+            System.out.println("Dimensions of matrix not matched");
+            //return 0;
+        }
+        this.n=c.n;
+        this.A = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < this.n; k++) {
+                    A[i][j] += this.A[i][k]*c.A[k][j];
+                }
+            }
+        }
+            return this;
     }
 }
